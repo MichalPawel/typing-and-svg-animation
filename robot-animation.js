636f1205fd5fd57058e3034bae2bc833562f0273
Pawel Michalski <pawelmm96@gmail.com>
2021-11-28 17:22:21 +0100

    add animations declarations

---
+++ robot-animation.js
@@ -0,0 +1,15 @@
+const bars = () => {
+    const tl = new gsap.timeline();
+}
+
+const blink = () => {
+    const tl = new gsap.timeline();
+}
+
+const move = () => {
+    const tl = new gsap.timeline();
+}
+
+
+//Master timeline
+const master = new gsap.timeline();
