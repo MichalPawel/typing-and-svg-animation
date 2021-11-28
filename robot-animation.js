0ed222b15ef590f92ef76711bc68b2d614169087
Pawel Michalski <pawelmm96@gmail.com>
2021-11-28 20:00:58 +0100

    add move

--- robot-animation.js
+++ robot-animation.js
@@ -20,11 +20,15 @@ const blink = () => {
     const tl = new gsap.timeline();
 }
 
-const move = () => {
-    const tl = new gsap.timeline();
+const move = (elements) => {
+    const tl = new gsap.timeline({ onComplete: () => move(document.querySelectorAll('#leg-left, #leg-right')) });
+    tl.to(elements, 1, { y: -100, stagger: 2, repeat: 0, yoyo: false })
+    tl.to(elements, 1, { y: 0, stagger: 2, repeat: 0, yoyo: false }, 1)
+
 }
 
 
 //Master timeline
 const master = new gsap.timeline();
 master.add(bars())
+master.add(move(document.querySelectorAll('#leg-left, #leg-right')))
